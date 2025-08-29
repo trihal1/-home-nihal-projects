66abbe04b9e4852e448372ad00bc93ec4eb92679
/𝔥𝔬𝔪𝔢/𝔫𝔦𝔥𝔞𝔩 <162717905+trihal1@users.noreply.github.com>
2025-08-29 16:23:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+# idk wut im doin
+import time
 
+number = input("ENTER NUMBER OF 67 S to MAKE")
+print(f"69 WILL BE REEPEATED {number} times!!!!!!!!")
+
+for num in number:
+  print ("69")
+
+# hehehehehe
